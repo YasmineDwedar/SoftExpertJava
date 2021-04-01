b4f4aa001cc8856b0647f73d1252bf5d3c5a0c79
YasmineDwedar <yasminedwedar30@gmail.com>
2021-04-01 02:27:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/softexpertjava/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.example.softexpertjava.utils;
+
+public class Constants {
+     private static final String BASE_URL="http://demo1585915.mockable.io/api/v1/";
+    private static final String cars="cars";
+}
